cb0c577e8f0b41fc1700cea6e688286f6152e4b2
Mario Parra de Miguel <mario.parra@soprasteria.com>
2019-12-18 11:52:19 +0100

    lite version without RHsso and 3Scale

--- server.js
+++ server.js
@@ -7,35 +7,78 @@ var fs = require('fs');
 var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
-var Keycloak = require('keycloak-connect');
-var cors = require('cors');
 var api = require('./lib/api');
 var app = express();
+app.set('json spaces', 4);
 const dotenv = require('dotenv');
 dotenv.config();
-app.use(bodyParser.json());
-app.use(cors());
-app.set('json spaces', 4);
-var memoryStore = new session.MemoryStore();
-var SSOSecret = process.env.SSOSecret;
-app.use(session({
-  secret: SSOSecret,
-  resave: false,
-  saveUninitialized: true,
-  store: memoryStore
-}));
-var keycloak = new Keycloak({ store: memoryStore });
+if (process.env.SSOEnable === "true"){  
+  app.use(bodyParser.json());
+  app.use(cors());
+  app.use(session({
+    secret: SSOSecret,
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore
+  }));
+  var memoryStore = new session.MemoryStore();
+  var SSOSecret = process.env.SSOSecret;
+  var Keycloak = require('keycloak-connect');
+  var cors = require('cors');
+  var keycloak = new Keycloak({ store: memoryStore });
+  app.use(keycloak.middleware({
+    logout: '/logout',
+    admin: '/'
+  }));
+  app.get("/events", keycloak.protect(), (req, res) => {
+    res.sendFile(path.join(__dirname, 'events.html')); 
+    nspevents.on('connection', function(socket){
+      events.getEvents(RHVserver);
+    });
+  });
+  app.get('/', keycloak.protect(), (req, res) => {
+    res.redirect('/events');
+  });
+  app.get('/metrics_menu', keycloak.protect(), (req, res) => {
+    res.sendFile(path.join(__dirname, 'metrics_menu.html'));
+  });
+  app.get('/api_menu', keycloak.protect(), (req, res) => {
+    res.sendFile(path.join(__dirname, 'api_menu.html'));
+  });
+  app.get('/about', keycloak.protect(), (req, res) => {
+    res.sendFile(path.join(__dirname, 'about.html'));
+  });
+}else{
+ // SSO Disabled
+
+ app.get("/events", (req, res) => {
+  res.sendFile(path.join(__dirname, 'events.html')); 
+  nspevents.on('connection', function(socket){
+    events.getEvents(RHVserver);
+  });
+});
+app.get('/', (req, res) => {
+  res.redirect('/events');
+});
+ app.get('/metrics_menu', (req, res) => {
+  res.sendFile(path.join(__dirname, 'metrics_menu.html'));
+});
+app.get('/api_menu', (req, res) => {
+  res.sendFile(path.join(__dirname, 'api_menu.html'));
+});
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'about.html'));
+});
+}
+
 app.use(express.static(path.join(__dirname, 'static')));
-app.use(keycloak.middleware({
-  logout: '/logout',
-  admin: '/'
-}));
+
 app.use('/api', api);
 // Load RHV Server or set to simulator default
-var RHVserver = process.env.RHVserver || "http://localhost:3001" ;//"https://rhvm-b5f6.rhpds.opentlc.com" example for opentlc labs, "http://localhost:3001" for localhost API simulator
+var RHVserver = process.env.RHVserver || "http://127.0.0.1:3001" ;//"https://rhvm-b5f6.rhpds.opentlc.com" example for opentlc labs, "http://127.0.0.1:3001" for localhost API simulator
 // If RHVtest = true then RHV Server to simulator defaults
-if (process.env.RHVtest){
-  var RHVserver = "https://localhost:3001" ;
+if (process.env.RHVtest == "true"){
+  var RHVserver = "https://127.0.0.1:3001" ;
 }
 // Load variable to enable record api outputs to use in simulator
 global.RHVrecord = process.env.RHVrecord || false ;
@@ -49,33 +92,43 @@ if(process.env.RHVCredentials == ""){
 const websocket = require('./websocket');
 const server = require('http').createServer(app);
 server.listen(8080);
+console.log("listening on port 8080");
 const io = websocket.init(server);
 const nspevents = io.of('/events');
 const nspProm2Nagios = io.of('/prom2nagios');
 const nspThresholds = io.of('/thresholds');
-app.get("/events", keycloak.protect(), (req, res) => {
-  res.sendFile(path.join(__dirname, 'events.html')); 
-  nspevents.on('connection', function(socket){
-    events.getEvents(RHVserver);
-  });
-});
-app.get('/', keycloak.protect(), (req, res) => {
-  res.redirect('/events');
-});
+
 if (process.env.NagiosCFGBuilder){
   var nagios = require('./lib/nagios');
-  app.get('/prom2nagios', keycloak.protect(), (req, res) => {
-    res.sendFile(path.join(__dirname, 'prom2nagios.html'));
-  });
-  app.get('/thresholds', keycloak.protect(), (req, res) => {
-    res.sendFile(path.join(__dirname, 'thresholds.html'));
-    nspThresholds.on('connection', function(socket){
-      db.readAllThresholds(socket);
-      socket.on('updateVal', function(data){
-        db.updateThreshold(data);
+
+  if (process.env.SSOEnable === "true"){
+    app.get('/prom2nagios', keycloak.protect(), (req, res) => {
+      res.sendFile(path.join(__dirname, 'prom2nagios.html'));
+    });
+    app.get('/thresholds', keycloak.protect(), (req, res) => {
+      res.sendFile(path.join(__dirname, 'thresholds.html'));
+      nspThresholds.on('connection', function(socket){
+        db.readAllThresholds(socket);
+        socket.on('updateVal', function(data){
+          db.updateThreshold(data);
+        });
       });
     });
-  });
+  } else {
+    app.get('/prom2nagios', (req, res) => {
+      res.sendFile(path.join(__dirname, 'prom2nagios.html'));
+    });
+    app.get('/thresholds', (req, res) => {
+      res.sendFile(path.join(__dirname, 'thresholds.html'));
+      nspThresholds.on('connection', function(socket){
+        db.readAllThresholds(socket);
+        socket.on('updateVal', function(data){
+          db.updateThreshold(data);
+        });
+      });
+    });
+  }
+  
   app.get('/nagios_cfg/:file', (req, res) => {
     if(fs.existsSync(path.join(__dirname, '/nagios_cfg/', req.params.file))){
       var aliasfileArr =  req.params.file.split("_");
@@ -86,15 +139,7 @@ if (process.env.NagiosCFGBuilder){
 app.get('/metrics', (req, res) => {
   metrics.sendMetricsRoute(res);
 });
-app.get('/metrics_menu', keycloak.protect(), (req, res) => {
-  res.sendFile(path.join(__dirname, 'metrics_menu.html'));
-});
-app.get('/api_menu', keycloak.protect(), (req, res) => {
-  res.sendFile(path.join(__dirname, 'api_menu.html'));
-});
-app.get('/about', keycloak.protect(), (req, res) => {
-  res.sendFile(path.join(__dirname, 'about.html'));
-});
+
 // Interval to query metrics in ms
 var interval = 60000;
 setInterval(function(){
